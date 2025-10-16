61c950069c8baf9847e7f997146866877526b071
Alassiri1 <slimanassiri3@gmail.com>
2025-10-16 22:06:42 +0300

    Update main.py

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,3 +1,23 @@
+# constants for coloring output
+COLORS = {
+    "green": "\033[92m",    # Green: results
+    "yellow": "\033[93m",   # Yellow: titles, warnings
+    "red": "\033[91m",      # Red: erors
+    "reset": "\033[0m"      # Reset: back to normal
+}
+
+
+def cprint(text, kind):
+    """Prints text in a specific color."""
+    if kind in COLORS:
+        color_code = COLORS[kind]
+    else:
+        color_code = COLORS["reset"]
+        
+    print(f"{color_code}{text}{COLORS['reset']}")
+
+
+
 import standard
 import programmer
 import scientific
@@ -5,12 +25,12 @@ import converter
 
 def main():
     while True:
-        print("\n=== Welcome to Multi-Mode Calculator ===")
+        cprint("\n=== Welcome to Multi-Mode Calculator ===","yellow")
         print("1. Standard Mode")
         print("2. Programmer Mode")
         print("3. Scientific Mode")
         print("4. Converter Mode")
-        print("5. Exit")
+        cprint("5. Exit","red")
 
         choice = input("Enter your choice (1-5): ")
 
@@ -23,10 +43,10 @@ def main():
         elif choice == "4":
             converter.main_menu()
         elif choice == "5":
-            print("Exiting... Goodbye!")
+            cprint("Exiting... Goodbye!","green")
             break
         else:
-            print("Invalid input. Please enter a number between 1 and 5.")
+            print("Invalid input. Please enter a number between 1 and 5.","red")
 
 if __name__ == "__main__":
     main()
